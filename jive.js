2bacbf9a284cd4488e7d9d5a5a44cdf8c1eda328
horsellcommon <crawfordj716@gmail.com>
2022-09-15 17:31:10 +0100

    images

--- jive.js
+++ jive.js
@@ -1,5 +1,8 @@
 const newGame = document.getElementById("newgame")
 const whackButton = document.getElementById("whack-button")
-
-newGame.addEventListener("click", () => {})
+const molePics = document.getElementsByClassName("chancemole")
+whackButton.disabled = true
+newGame.addEventListener("click", () => {
+    whackButton.disabled = false
+})
 whackButton.addEventListener("click", () => {})
